0e10661d6646c936493b07e801f192a283733479
onas <iriajenfrancis@gmail.com>
2022-10-16 15:07:20 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - Prints "and that piece of art is useful" - - Dora Korpar
  * followed by a new line, to standard error.
@@ -7,7 +9,7 @@
 
 int main(void)
 {
-	write(2,
+	write(STDOUT_FILENO,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
